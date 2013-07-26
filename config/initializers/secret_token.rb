298217f814c5975210a6e31635e0f562128adb2a
kitahara-masahiro <kitahara_masahiro@nexway.co.jp>
2013-07-26 21:35:58 +0900

    Initial commit to the Baseball Project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baseball::Application.config.secret_token = '1bdbb83b22beddbdc82b0e9286e4790b02d6b9e4d53f6593142d614d7507db7a1604f3295038c304cde488527fa9b4de92424eb31f9c7b021f8a7985f01b9405'
